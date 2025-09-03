740358b66385f51ff0638a0dd4195c0fe4677fdd
Park Gwonyong <rnjsdydqkr@naver.com>
2025-09-03 16:39:00 +0900

    image

--- SwiftUI_Practice/ContentView.swift
+++ SwiftUI_Practice/ContentView.swift
@@ -10,19 +10,18 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        ZStack {
-            Image("1")
-                .resizable()
-                .edgesIgnoringSafeArea(.all)
-                .aspectRatio(contentMode: .fill)
-            VStack {
-                Spacer()
-                Text("Hello Picture")
-                    .font(.largeTitle)
-                    .foregroundStyle(.white)
-                    .offset(x: 90, y: 0)
-            }
-        }
+        ZStack(alignment: .bottomTrailing) {
+            Color.clear
+            Text("Hello Picture")
+                .font(.largeTitle)
+                .foregroundStyle(.black)
+                .padding(.trailing, 10)
+        }.background(
+             Image("2")
+                .resizable(capInsets: .init(top: 100, leading: 0, bottom: 0, trailing: 0), resizingMode: .stretch)
+//                 .edgesIgnoringSafeArea(.all)
+//                 .aspectRatio(contentMode: .fit)
+        )
             
     }
 }
